558d0566773a0146b153d78908198c6e94c1e9e4
Manpreet Singh <manpreetmss400@gmail.com>
2024-06-14 17:28:22 +0530

    improved styles for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "./components/mode-toggle";
 import { Menu } from "./components/Menu";
 
 export default function Home() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <div className="flex justify-end">
-        <ModeToggle />
-      </div>
       <Menu />
     </ThemeProvider>
   );
