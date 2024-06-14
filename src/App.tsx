7385df92ea184ee81c80d8eed664147c88d0be64
Manpreet Singh <manpreetmss400@gmail.com>
2024-06-14 17:40:43 +0530

    Accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { Menu } from "./components/Menu";
+import { AccordionSection } from "./components/accodrion";
 
 export default function Home() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <Menu />
+
+      <div className="flex justify-center p-8">
+        <AccordionSection />
+      </div>
     </ThemeProvider>
   );
 }
